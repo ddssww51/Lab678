07b17a92eef1db207ce478eb55ec1d93fa63a0b9
„Mikołaj” <mikolaj.p00@outlook.com>
2023-06-13 23:11:42 +0200

    Program do konwersji danych obsługujący formaty

--- Program.py
+++ Program.py
@@ -1,64 +1,91 @@
 import argparse
-import string
 import json
 import yaml
 import xml.etree.ElementTree as ET
 
-parser = argparse.ArgumentParser(description='Konwersja plików XML, JSON i YAML.')
+def convert_file(input_file, output_file):
+    input_file_extension = input_file.split('.')[-1].lower()
+    output_file_extension = output_file.split('.')[-1].lower()
 
-parser.add_argument('input_file', type=str, help='Nazwa pliku wejściowego.')
-parser.add_argument('output_file', type=str, help='Nazwa pliku wyjściowego.')
-parser.add_argument('format', type=str, help='Format pliku')
+    if input_file_extension == 'json':
+        try:
+            with open(input_file, 'r') as file:
+                json_data = json.load(file)
 
-args = parser.parse_args()
+            if output_file_extension == 'xml':
+                root = ET.Element('root')
+                for key, value in json_data.items():
+                    child = ET.SubElement(root, key)
+                    child.text = str(value)
+                tree = ET.ElementTree(root)
+                tree.write(output_file)
+            elif output_file_extension in ('yml', 'yaml'):
+                with open(output_file, 'w') as file:
+                    yaml.dump(json_data, file)
 
-input_file_extension = args.input_file.split('.')[-1].lower()
+            print("Konwersja zakończona sukcesem.")
 
-print("Plik wejściowy:", args.input_file)
-print("Plik wyjściowy:", args.output_file)
-print("Format:", args.format)
-print("Rozszerzenie pliku wejściowego:", input_file_extension)
+        except FileNotFoundError:
+            print("Plik wejściowy nie został znaleziony.")
+        except json.JSONDecodeError:
+            print("Nieprawidłowa składnia JSON.")
 
-if input_file_extension == 'json':
-    try:
-        with open(args.input_file, 'r') as file:
-            json_data = json.load(file)
-        print("Składnia pliku jest poprawna.")
+    elif input_file_extension in ('yml', 'yaml'):
+        try:
+            with open(input_file, 'r') as file:
+                yaml_data = yaml.safe_load(file)
 
-        with open(args.output_file, 'w') as file:
-            json.dump(json_data, file)
-        print("Dane zostały zapisane do pliku w formacie JSON.")
+            if output_file_extension == 'xml':
+                root = ET.Element('root')
+                for key, value in yaml_data.items():
+                    child = ET.SubElement(root, key)
+                    child.text = str(value)
+                tree = ET.ElementTree(root)
+                tree.write(output_file)
+            elif output_file_extension == 'json':
+                with open(output_file, 'w') as file:
+                    json.dump(yaml_data, file)
 
-    except FileNotFoundError:
-        print("Plik wejściowy nie został znaleziony.")
-    except json.JSONDecodeError:
-        print("Nieprawidłowa składnia JSON.")
+            print("Konwersja zakończona sukcesem.")
 
-elif input_file_extension == 'yml' or input_file_extension == 'yaml':
-    try:
-        with open(args.input_file, 'r') as file:
-            yaml_data = yaml.safe_load(file)
-        print("Składnia pliku jest poprawna.")
+        except FileNotFoundError:
+            print("Plik wejściowy nie został znaleziony.")
+        except yaml.YAMLError as exc:
+            print("Nieprawidłowa składnia YAML: {}".format(exc))
 
-        with open(args.output_file, 'w') as file:
-            yaml.dump(yaml_data, file)
-        print("Dane zostały zapisane do pliku w formacie YAML.")
+    elif input_file_extension == 'xml':
+        try:
+            tree = ET.parse(input_file)
+            root = tree.getroot()
 
-    except FileNotFoundError:
-        print("Plik wejściowy nie został znaleziony.")
-    except yaml.YAMLError as exc:
-        print("Nieprawidłowa składnia YAML:", exc)
+            if output_file_extension == 'json':
+                data = {}
+                for element in root:
+                    data[element.tag] = element.text
+                with open(output_file, 'w') as file:
+                    json.dump(data, file)
+            elif output_file_extension in ('yml', 'yaml'):
+                data = {}
+                for element in root:
+                    data[element.tag] = element.text
+                with open(output_file, 'w') as file:
+                    yaml.dump(data, file)
 
-elif input_file_extension == 'xml':
-    try:
-        tree = ET.parse(args.input_file)
-        root = tree.getroot()
-        print("Składnia pliku jest poprawna.")
+            print("Konwersja zakończona sukcesem.")
 
-        tree.write(args.output_file)
-        print("Dane zostały zapisane do pliku w formacie XML.")
+        except FileNotFoundError:
+            print("Plik wejściowy nie został znaleziony.")
+        except ET.ParseError as exc:
+            print("Nieprawidłowa składnia XML: {}".format(exc))
 
-    except FileNotFoundError:
-        print("Plik wejściowy nie został znaleziony.")
-    except ET.ParseError as exc:
-        print("Nieprawidłowa składnia XML:", exc)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Program do konwersji danych obsługujący formaty: .xml, .json i .yml (.yaml)')
+    parser.add_argument('input_file', help='Ścieżka do pliku wejściowego')
+    parser.add_argument('output_file', help='Ścieżka do pliku wyjściowego')
+
+    args = parser.parse_args()
+
+    input_file = args.input_file
+    output_file = args.output_file
+
+    convert_file(input_file, output_file)
